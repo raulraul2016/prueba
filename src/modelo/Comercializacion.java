e68de8ea8f5436a3068f4a7528963b3b0d0c6736
bangho <bangho@bangho>
2016-09-26 09:23:05 -0300

    Actualizado 20-09-2016

--- src/modelo/Comercializacion.java
+++ src/modelo/Comercializacion.java
@@ -11,4 +11,40 @@ package modelo;
  */
 public class Comercializacion {
     
+    private Long id_comercializacion;
+    private String destino_produccion;
+    private String descripcion;
+
+    public Comercializacion(Long id_comercializacion, String destino_produccion, String descripcion) {
+        this.id_comercializacion = id_comercializacion;
+        this.destino_produccion = destino_produccion;
+        this.descripcion = descripcion;
+    }
+
+    public Long getId_comercializacion() {
+        return id_comercializacion;
+    }
+
+    public void setId_comercializacion(Long id_comercializacion) {
+        this.id_comercializacion = id_comercializacion;
+    }
+
+    public String getDestino_produccion() {
+        return destino_produccion;
+    }
+
+    public void setDestino_produccion(String destino_produccion) {
+        this.destino_produccion = destino_produccion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+    
 }
